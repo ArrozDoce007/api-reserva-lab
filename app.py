51f22c70b13af07cb67ff2d4e5086cdad6d6d9ca
Guilherme Sousa <143344186+ArrozDoce007@users.noreply.github.com>
2024-06-15 19:10:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def deletar_contato(id):
     return jsonify({'message': 'Contato deletado com sucesso!'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0' port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
